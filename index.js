82f2abc9d4a0617d0ad155e99feb37c3f6be4056
John Watt <j.watt10@gmail.com>
2019-10-30 13:26:27 -0700

    mvp

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
-const server = express();
-const postsRouter = require('./posts/posts-router.js')
 
+const postsRouter = require('./posts/posts-router.js')
+const server = express();
+
+server.use(express.json());
 server.get('/api/posts', postsRouter);
 
-server.listen(8000, () => {
-    console.log('\n*** Server Running on http://localhost:8000 ***\n');
+server.get("/", (req, res) => {
+  res.send(`
+    
+    <p>testing API</p>
+  `);
+});
+server.use("/api/posts", postsRouter);
+
+server.listen(5000, () => {
+    console.log('\n*** Server Running on http://localhost:5000 ***\n');
   });
+
